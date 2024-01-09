e3ee6ad62492c067264d4332a2961f3dd05b57ba
kaanakdogan <kaan.akdogann@gmail.com>
2024-01-10 01:11:26 +0300

    popover

--- src/app/components/passangerPicker.tsx
+++ src/app/components/passangerPicker.tsx
@@ -1,9 +1,28 @@
+import { useRef, useState } from 'react';
+import { Popover } from './popover';
 import { Person } from './svg/person';
 
 export function PassangerPicker() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
   return (
-    <button className='m-1 flex h-10 w-14 items-center justify-center bg-[#242A38] p-2 text-white'>
-      <Person className='fill-gray-50' />{' '}
-    </button>
+    <>
+      <button
+        ref={buttonRef}
+        className='m-1 flex h-10 w-14 items-center justify-center bg-[#242A38] p-2 text-white'
+        onClick={() => setIsOpen(true)}
+      >
+        <Person className='fill-gray-50' />{' '}
+      </button>
+      <Popover
+        parentRef={buttonRef}
+        isOpen={isOpen}
+        close={() => setIsOpen(false)}
+      >
+        <div>Test</div>
+      </Popover>
+    </>
   );
 }
--- src/app/components/popover.tsx
+++ src/app/components/popover.tsx
@@ -1,1 +1,76 @@
-export function Popover() {}
+import { useLayoutEffect, useRef, useState } from 'react';
+import { createPortal } from 'react-dom';
+
+type PopoverProps = {
+  isOpen: boolean;
+  children: React.ReactNode;
+  parentRef: React.RefObject<HTMLElement>;
+  close: () => void;
+};
+
+type PositionType = {
+  top: string;
+  left: string;
+};
+
+function PopoverContent({
+  children,
+  close,
+  parentRef,
+}: {
+  children: React.ReactNode;
+  close: () => void;
+  parentRef: React.RefObject<HTMLElement>;
+}) {
+  const contentRef = useRef<HTMLDivElement>(null);
+  const [position, setPosition] = useState<PositionType>({
+    top: '0',
+    left: '0',
+  });
+
+  useLayoutEffect(() => {
+    if (parentRef.current && contentRef.current) {
+      const rect = parentRef.current.getBoundingClientRect();
+      const top = rect.top + rect.height + 2;
+      const contentRect = contentRef.current.getBoundingClientRect();
+      const left = rect.left + rect.width / 2 - contentRect.width / 2;
+      setPosition({ top: `${top}px`, left: `${left}px` });
+    }
+
+    const clickListener = (e: MouseEvent) => {
+      console.log(e);
+      if (e.target !== contentRef.current) {
+        close();
+      }
+    };
+    document.body.addEventListener('click', clickListener);
+
+    return () => {
+      document.body.removeEventListener('click', clickListener);
+    };
+  }, []);
+
+  return (
+    <div
+      ref={contentRef}
+      style={{ top: position.top, left: position.left }}
+      className='size-50 absolute bg-white'
+    >
+      {children}
+    </div>
+  );
+}
+
+export function Popover({ isOpen, children, close, parentRef }: PopoverProps) {
+  if (isOpen) {
+    console.log(parentRef.current);
+    return createPortal(
+      <PopoverContent close={close} parentRef={parentRef}>
+        {children}
+      </PopoverContent>,
+      document.body
+    );
+  }
+
+  return null;
+}
