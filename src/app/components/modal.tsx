015d5ab3277bb0009d3726c4e6df893de966bb79
kaanakdogan <kaan.akdogann@gmail.com>
2024-01-10 15:46:51 +0300

    modal component added

---
+++ src/app/components/modal.tsx
@@ -0,0 +1,30 @@
+import { createPortal } from 'react-dom';
+
+export function Modal({
+  isOpen,
+  children,
+  onClose,
+}: {
+  isOpen: boolean;
+  children: React.ReactNode;
+  onClose: () => void;
+}) {
+  if (!isOpen) {
+    return null;
+  }
+
+  return createPortal(
+    <div className='absolute left-0 top-0 h-screen w-screen bg-gray-600/50'>
+      <div className='m-auto flex h-40 w-60 translate-y-full flex-col rounded-lg bg-white opacity-100'>
+        <button
+          className='w-5 self-end rounded rounded-tr-lg bg-[#E81932] text-white'
+          onClick={onClose}
+        >
+          x
+        </button>
+        <div className='p-3'>{children}</div>
+      </div>
+    </div>,
+    document.body
+  );
+}
--- src/app/components/selection.tsx
+++ src/app/components/selection.tsx
@@ -5,6 +5,7 @@ import { LocationInput } from './locationInput';
 import { Date } from './date';
 import { PassangerPicker } from './passangerPicker';
 import { getFlights } from '../actions';
+import { Modal } from './modal';
 
 export function Selection() {
   const [arrival, setArrival] = useState<string>('');
@@ -13,6 +14,8 @@ export function Selection() {
   const [checkedCabin, setCheckedCabin] = useState<'ECONOMY' | 'BUSINESS'>(
     'ECONOMY'
   );
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [error, setError] = useState<string>('');
 
   useEffect(() => {
     console.log('arrival', arrival);
@@ -22,27 +25,35 @@ export function Selection() {
   const onSubmit = () => {
     getFlights(arrival, departure)
       .then((a) => console.log(a))
-      .catch((e) => console.log(e));
+      .catch((e) => {
+        setError(e.message);
+        setIsModalOpen(true);
+      });
   };
 
   return (
-    <div className='bg-selectionbg align-center flex flex-wrap justify-center p-5'>
-      <LocationInput
-        isArrival={false}
-        value={departure}
-        onChange={setDeparture}
-      />
-      <LocationInput isArrival={true} value={arrival} onChange={setArrival} />
-      <Date />
-      <PassangerPicker
-        checkedCabin={checkedCabin}
-        setCheckedCabin={setCheckedCabin}
-        passangerCount={passangerCount}
-        setPassangerCount={setPassangerCount}
-      />
-      <button className='m-1 w-5 bg-[#E81932] text-white' onClick={onSubmit}>
-        {'>'}
-      </button>
-    </div>
+    <>
+      <div className='bg-selectionbg align-center flex flex-wrap justify-center p-5'>
+        <LocationInput
+          isArrival={false}
+          value={departure}
+          onChange={setDeparture}
+        />
+        <LocationInput isArrival={true} value={arrival} onChange={setArrival} />
+        <Date />
+        <PassangerPicker
+          checkedCabin={checkedCabin}
+          setCheckedCabin={setCheckedCabin}
+          passangerCount={passangerCount}
+          setPassangerCount={setPassangerCount}
+        />
+        <button className='m-1 w-5 bg-[#E81932] text-white' onClick={onSubmit}>
+          {'>'}
+        </button>
+      </div>
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        {error}
+      </Modal>
+    </>
   );
 }
