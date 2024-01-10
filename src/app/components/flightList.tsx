0edc49491b7aecefa89404a94ff425a65a84b766
kaanakdogan <kaan.akdogann@gmail.com>
2024-01-10 23:14:06 +0300

    toggle fixes

--- src/app/components/flightList.tsx
+++ src/app/components/flightList.tsx
@@ -6,17 +6,17 @@ import { FlightListItem } from './flightListItem';
 import { Toggle } from './toggle';
 
 export function FlightList({ data }: { data: FlightType[] }) {
-  const [toggleValue, setToggleValue] = useState<string>('');
+  const [toggleValue, setToggleValue] = useState<boolean>(false);
   return (
     <>
       <div className='self-start pt-3'>
         <Toggle
           label='Promosyon Kodu'
-          onChange={(e) => {
-            console.log('onChange', e.target.value);
-            setToggleValue(e.target.value);
+          onChange={(value) => {
+            console.log('onChange', value);
+            setToggleValue(value);
           }}
-          value={toggleValue}
+          enabled={toggleValue}
         />
       </div>
       <div className='shadow-list flex w-full flex-col rounded pt-5'>
@@ -31,7 +31,11 @@ export function FlightList({ data }: { data: FlightType[] }) {
         </div>
         <div className='rounded-b border-2 border-t-0 border-solid border-[#EAEAEA] bg-[#F9F9F9] p-3'>
           {data.map((item, index) => (
-            <FlightListItem key={index} item={item} />
+            <FlightListItem
+              key={index}
+              item={item}
+              isPromoActivated={toggleValue}
+            />
           ))}
         </div>
       </div>
--- src/app/components/toggle.tsx
+++ src/app/components/toggle.tsx
@@ -1,25 +1,30 @@
 export function Toggle({
   label,
   onChange,
-  value,
+  enabled,
 }: {
   label: string;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  value: string;
+  onChange: (e: boolean) => void;
+  enabled: boolean;
 }) {
   return (
     <div className='flex'>
-      <div className='pr-5 text-center text-sm'>
+      <span className='mr-2 text-sm font-medium text-gray-900'>
         <b>{label}</b>
-      </div>
-      <label className='relative inline-flex cursor-pointer items-center'>
+      </span>
+      <label className='relative mr-5 inline-flex cursor-pointer items-center'>
         <input
           type='checkbox'
-          value={value}
           className='peer sr-only'
-          onChange={onChange}
+          checked={enabled}
+          readOnly
         />
-        <div className="peer h-5 w-11 rounded-full bg-gray-300 after:absolute after:start-[2px] after:top-[2px] after:h-4 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white rtl:peer-checked:after:-translate-x-full dark:border-gray-600 "></div>
+        <div
+          onClick={() => {
+            onChange(!enabled);
+          }}
+          className="peer h-6 w-11 rounded-full bg-gray-200  after:absolute  after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-green-300"
+        ></div>
       </label>
     </div>
   );
